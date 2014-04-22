37c03019d7d538e2b7896a33dae5a382d05f2225
Søren Ranneries <spinkelben@gmail.com>
2014-04-22 21:51:38 +0200

    Medelling with the data parser

--- webinterface/webserver/server_files/data_parser.js
+++ webinterface/webserver/server_files/data_parser.js
@@ -5,7 +5,7 @@ var startSequence = [255, 123, 10];		//Signature of the start of a can frame?
 /* Checks three databytes, and returns true if they contain
  * a start sequence.
  * Input: An array containing three bytes 
- * OutPut: True if the three bytes is a start sequence, False if not. 
+ * OutPut: True if the three bytes matches the start sequence, False if not. 
  */
 function isStartSequence(data) {
 
@@ -29,26 +29,46 @@ function isStartSequence(data) {
 	}*/
 }
 
+
+
 module.exports = function(data) {
 	// Data from serialport
 	var datain = data;
+	//Output
+	var valOut = 0;
+	//Size of remaining package bytes
+	var bytesToRead = -1;
+
+	//Processes each data byte
+	while (datain.length !== 0) {
+		var startSequenceLength = 3;
+		//Looks for a start sequence
+		if (isStartSequence(datain.slice(0,startSequenceLength))) {
+
+		}
+		else {
+			datain.Shift()
+		}
+	}
 
 	//Loops through each byte in data stream
 	for(var i=0; i<datain.length; i++) {
 		var currByte = datain[i]; // the current byte in the stream
 		var startSequenceLength = 3;
 
-		/*Looks for start sequence */
+		/*Looks for start sequence*/
 		if (isStartSequence(datain.slice(i, i + startSequenceLength))) {
 			package_start = true;
-			//Start sequence found, skip the start sequence bytes
+			//Start sequence found, move the index past the start sequence bytes
 			i += startSequenceLength;
 			currByte = datain[i];
+
+
 		}
 
-		//Begin Legacy code *********************************************************
+		//Begin Legacy code /*********************************************************
 
-		//console.log("("+currByte+")");
+		////console.log("("+currByte+")");
 		
 		// Search data pack. start sequence, if found then next byte is a type
 		/*if((package_start_counter === 0) && (currByte === startSequence[0]))
@@ -63,14 +83,13 @@ module.exports = function(data) {
 		
 		//End legacy code************************************************************* 
 
-		// Packet start found, get packet ID
+		// Packet start found, get packet ID and size
 		if (package_start){	
 		
 			// Reset
 			package_start = false;
-			bytesToRead = -1;
 			valOut = 0;
-			
+			//Get data type key
 			dataTypeKey = getDataType(dataType,currByte);
 			
 			// Valid data type found
@@ -78,11 +97,11 @@ module.exports = function(data) {
 				bytesToRead = (dataType[dataTypeKey].datalength/8); // Bytes to read
 			}
 			else
-				console.log("Invalid data (ID: "+currByte+")");
+				console.error("Invalid data (ID: "+currByte+")");
 			continue;
 		}			
 		
-		// Data bytes 
+		// Read Data bytes 
 		if(bytesToRead > 0){	
 			valOut = valOut + (currByte << (8*(bytesToRead-1)));	// Shift bytes
 			bytesToRead -= 1; // Databyte counter
@@ -91,9 +110,11 @@ module.exports = function(data) {
 	
 		// No more data bytes, 
 		if(bytesToRead === 0){
-			var nameTerm = dataType[dataTypeKey].name.rpad(" ", 10); // Dette skal ikke ske
+			
 			var name = dataType[dataTypeKey].name;
 			var  value = dataType[dataTypeKey].conv(valOut);
+
+			//Format value to range
 			value = Math.min(value, dataType[dataTypeKey].max);
 			value = Math.max(value, dataType[dataTypeKey].min);
 
@@ -108,7 +129,7 @@ module.exports = function(data) {
 			if(dataType[dataTypeKey].active === 1){
 				// Add to data pack
 				dataTx[dataCounter++] = sensor;
-				//console.log("ID:\t"+dataType[dataTypeKey].ID+"\tType:\t"+nameTerm+"\tData:\t"+value);
+
 			}
 			
 			// Reset
