72fe0a575c8dbf18053bc2aa470502bfdfac0139
Frederik Bruhn Mikkelsen <frederik310@gmail.com>
2014-09-20 22:03:27 +0200

    Added gitignore, since lots of generated files were commited. Also added build and clean script for building python wrapper for the canparser. Also copied test data from dropbox.

--- python/canparser.i
+++ python/canparser.i
@@ -3,7 +3,6 @@
 %{
 /* Put header files here or function declarations like below */
 #include "./canparser.h"
-#include <stdint.h>
 extern int parseNext(uint8_t dataByte, sensor_t *sensor, parser_t *p);
 void canfile2csv(const char *path);
 %}
--- python/receiver.py
+++ python/receiver.py
@@ -1,25 +1,37 @@
 import serial
 import canparser
 from ctypes import c_uint8, cast, POINTER
+import os
 
 #SERIAL_PORT = 
 
 
 #ser = serial.Serial(SERIAL_PORT)
 #print ser.name
-ser = open('./testdata', "rb")
-try:
-	rawdata = ser.read(1)
-	while rawdata != "":
-		# load data into c function return realdata
-		parser_t = canparser.parser_t()
-		sensor_t = canparser.sensor_t()
+for file in os.listdir("./testdata"):
+	ser = open("./testdata/{}".format(file), "rb")
+	try:
+		byteCount = 0
+		byte = ser.read(1)
+		while byte:
+			byteCount = byteCount + 1
+			# load data into c function return realdata
+			parser_t = canparser.parser_t()
+			sensor_t = canparser.sensor_t()
 
-		realdata = canparser.parseNext(cast(rawdata, POINTER(c_uint8)).contents.value, sensor_t, parser_t)
-		if parser_t.sensorFound: # We got data in sensor_t
-			print("id: {} name: {}: value: {}".format(sensor_t.id, sensor_t.name, sensor_t.value))
+			realdata = canparser.parseNext(cast(byte, POINTER(c_uint8)).contents.value, sensor_t, parser_t)
+			if parser_t.sensorFound: # We got data in sensor_t
+				print("id: {} name: {}: value: {}".format(sensor_t.id, sensor_t.name, sensor_t.value))
+				input()
+			#else:
+				#print(byteCount)
+			
+			byte = ser.read(1)
+			print(realdata)
+		break
+		print("Total byte count: {}".format(byteCount))
+		print("Press any key for next file")
+		input()
 
-		rawdata = ser.read(1)
-
-finally:
-	ser.close()
+	finally:
+		ser.close()
