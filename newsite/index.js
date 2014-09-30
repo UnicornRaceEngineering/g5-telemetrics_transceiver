71a5c6933785bf7a042a0d3ebdd200ebe78fa0b8
Frederik Bruhn Mikkelsen <frederik310@gmail.com>
2014-09-30 20:34:02 +0200

    started implementing data parsing

--- newsite/index.js
+++ newsite/index.js
@@ -1,16 +1,19 @@
-// init path so we can create paths using path.join
-var path = require('path');
+/* Includes/Imports */
+
+var path = require('path'); // init path so we can create paths using path.join()
 // init app using express to make app a function handler which we can parse on to the HTTP server
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-// Open a connection to a serial port
 var SerialPort = require('serialport').SerialPort
-var serialport = new SerialPort("/dev/ttyXXX", {
-	baudrate: XXXXXX
-});
 
-var clientsAmout = 0;
+/* Global variabels */
+// Open a connection to a serial port
+var serialport = new SerialPort("/dev/ttyUSB0", {
+	baudrate: 115200
+});
+var clientsAmout = 0; // Keep statistics of the amount of connected clients
+var startSequence = [0xA1, 0xB2, 0xC3, 0xD4, 0xE5, 0xF6];
 
 // Defining the route handler /
 app.get('/', function(request, response){
@@ -28,6 +31,7 @@ io.on('connection', function(socket){
 		clientsAmout--; // decrement amount of clients
 		console.log("a client disconnected");
 		console.log("Total: " + clientsAmout);
+	});
 });
 
 // We make the http server listen to port 3000
@@ -35,6 +39,14 @@ http.listen(3000, function(){
 	console.log('listening on *:3000');
 });
 
+/* Start sequence
+0xA01
+0xB02
+0xC03
+0xD04
+0xE05
+0xF06
+*/
 // the serial port is opened asynchronously, meaning we are not able to read data
 // before the 'open' event has happened.
 serialport.on('open', function(){
@@ -42,9 +54,39 @@ serialport.on('open', function(){
 	serialport.on('data', function(data){
 		// Data has been recieved.
 		// TODO: Handle data, using io.emit() to send data to clients.
-	});
-	serialport.write('ls\n', function(err, results){
-		console.log('err ' + err);
-		console.log('results ' + results);
+		//io.emit('rear wheel temp', data)
+		//console.log(data);
+		ParsePackage(data);
 	});
 });
+
+var ParsePackage = function(data){
+	var isStartPackageFound = false;
+	var startPackageCounter = 0; // keeps track of where we are in the start package
+
+	for(i = 0; i < data.length; i++){
+		var currentByte = data.readUInt8(i);
+		if (isStartPackageFound === false){
+			startPackageCounter = isPartOfStartPack(currentByte, startPackageCounter);
+			// if our counter is has same value as the length of the start sequence we are ready to receive a package
+			if (startPackageCounter === (startSequence.length - 1)){
+				isStartPackageFound = true;
+				console.log("Start package found!");
+			}
+			continue;
+			
+			console.log(currentByte);
+		} else {
+			//TODO: build buffer by reading id which is byte 1 and length which is byte 2 and 3
+		}
+	}
+};
+
+var isPartOfStartPack = function(currentByte, counter){
+	if (currentByte === startSequence[counter]){
+		counter++;
+	} else {
+		counter = 0;
+	}
+	return counter;
+};
