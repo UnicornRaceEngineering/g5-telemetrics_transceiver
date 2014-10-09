6cf6b5e3a591d690cf0f8e96ea7535e61b3d138a
Frederik Bruhn Mikkelsen <frederik310@gmail.com>
2014-10-09 19:01:35 +0200

    test packages can now be parsed

--- newsite/index.js
+++ newsite/index.js
@@ -51,127 +51,153 @@ http.listen(3000, function(){
 serialport.on('open', function(){
 	console.log('Serial port is now open');
 
+	// For debugging purpose
+	//while(1){
+		//var testPackage = new Buffer([0xA1, 0xB2, 0xC3, 0xD4, 0xE5, 0xF6, 0x0a, 0x0, 0x6, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6]);
+		//console.log(testPackage);
+		//ParsePackage(testPackage);
+	//}
 
 	serialport.on('data', function(data){
 		// Data has been recieved.
 		// TODO: Handle data, using io.emit() to send data to clients.
 		//io.emit('rear wheel temp', data)
-		//console.log(data);
+		console.log(data);
 		ParsePackage(data);
 	});
 });
 
 function Package(){
-	//init self
-	var self = this; // this makes us availble to refrence this object in inner functions.
-	
-	// private members
-	var _id = undefined;
-	var _buffer = undefined;
-	var _bufferIndex = 0;
-	var _startSequence = [0xA1, 0xB2, 0xC3, 0xD4, 0xE5, 0xF6];
-	var _startSequenceIndex = undefined;
-	var _isStartSequenceFound = undefined;
-	var _lengthBuffer = undefined;
-	var _lengthBufferIndex = undefined;
+	var self = this;
 
-	// constructor
-	this.ctor = function(){
-		self._bufferIndex = 0;
-		self._isStartSequenceFound = false;
-		self._startSequenceIndex = 0;
-		self._lengthBuffer = new Buffer(2);
-		self._lengthBufferIndex = 0;
-		console.log("constructor run");
-	}
+	// private members
+	self._id = undefined;
+	self._buffer = undefined;
+	self._bufferIndex = 0;
+	self._startSequence = [0xA1, 0xB2, 0xC3, 0xD4, 0xE5, 0xF6];
+	self._startSequenceIndex = 0;
+	self._isStartSequenceFound = false;
+	self._lengthBuffer = undefined;
+	self._lengthBufferIndex = 0;
 
 	// public methods
-	this.AddByte = AddByte;
+	self.AddByte = AddByte;
 	function AddByte(newByte){
-		if (self._isStartSequenceFound === false && isPartOfStartSequence(newByte)){
-			if (self._isStartSequenceIndex === self._startSequence.length){
+		if (self._isStartSequenceFound === false){
+			if (isPartOfStartSequence(newByte)){
+				self._startSequenceIndex++;
+				return;
+			} else if (self._startSequenceIndex >= self._startSequence.length - 1){
+				console.log(self._startSequenceIndex >= self._startSequence.length - 1);
 				self._isStartSequenceFound = true;
+				console.log("Start sequence found");
+			} else {
+				console.log("sequence index: " + self._startSequenceIndex);
 			}
-			return;
 		}
-		console.log("Start sequence found");
 
 		if (self._id === undefined){
 			self._id = newByte;
+			console.log("id: " + self._id);
 			return;
 		}
-		console.log("id found: " + self._id);
 
+		// Initialize the buffer containg the length of the package
 		if (self._lengthBuffer === undefined){
-			self._lengthBuffer = new Buffer(2);
+			self._lengthBuffer = new Buffer([0x0, 0x0]);
+			console.log("Log buffer Initialized");
 		}
-
-		if (self._lengthBufferIndex < self._lengthBuffer.length - 1){
+		
+		
+		// add the current byte to the length buffer, if length is not found yet
+		if (self._lengthBufferIndex <= self._lengthBuffer.length - 1){
+			console.log("Length byte" + self._lengthBufferIndex + ": " + newByte);
+			console.log("length index: " + self._lengthBufferIndex);
+			console.log(self._lengthBuffer);
 			self._lengthBuffer[self._lengthBufferIndex] = newByte;
 			self._lengthBufferIndex++;
 			return;
 		}
-		console.log("length found");
-		console.log(self._lengthBuffer);
+
 		if (self._buffer === undefined){
 			// get buffer length
-			n = self._lengthBuffer.readInt16BE(0);
+			var n = self._lengthBuffer.readUInt16BE(0)
+			console.log("Length of package: " + n);
 			self._buffer = new Buffer(n);
+			self._bufferIndex = 0;
 		}
-		console.log(self._lengthBuffer.length);
-		if (self._bufferIndex !== self._buffer.length - 1){
-			console.log(_bufferIndex);
+
+		if (self._bufferIndex <= self._buffer.length - 1){
 			self._buffer[self._bufferIndex] = newByte;
+			console.log("BufferIndex: " + self._bufferIndex);
+			console.log("byte added: " + newByte);
+			console.log(self._buffer);
 			self._bufferIndex++;
-			console.log("byte added");
 		} else {
-			console.log("bufferIndex out of range");
+			// package is full reset all values
+			self._id = undefined;
+			self._buffer = undefined;
+			self._bufferIndex = 0;
+			self._startSequence = [0xA1, 0xB2, 0xC3, 0xD4, 0xE5, 0xF6];
+			self._startSequenceIndex = 0;
+			self._isStartSequenceFound = false;
+			self._lengthBuffer = undefined;
+			self._lengthBufferIndex = 0;
+			console.log("bufferIndex out of range: " + self._bufferIndex);
 		}
-	}
-	this.GetId = GetId;
+	};
+	self.GetId = GetId;
 	function GetId(){
 		return self._id;
-	}
-	this.GetLength = GetLength;
+	};
+	self.GetLength = GetLength;
 	function GetLength(){
 		return self._buffer.length;
-	}
-	this.GetBuffer = GetBuffer;
+	};
+	self.GetBuffer = GetBuffer;
 	function GetBuffer(){
 		return self._buffer;
-	}
-	this.IsFull = IsFull;
+	};
+	self.IsFull = IsFull;
 	function IsFull(){
-		if (self._buffer === undefined)
+		if (self._buffer === undefined){
+			console.log("is full: buffer undefined");
 			return false;
+		}
 
-		if (self._bufferIndex === self._buffer.length - 1)
+		if (self._bufferIndex === self._buffer.length) {
+			console.log("Is full: true");
 			return true;
-		else
+		} else {
+			console.log("Is full: false");
 			return false;
-	}
+		}
+	};
 
 	// private methods
 	var isPartOfStartSequence = function(currentByte){
 		if (currentByte === self._startSequence[self._startSequenceIndex]){
-			self._startSequenceIndex++;
+			console.log("part of start sequence: " + self._startSequenceIndex);
 			return true;
 		}
-
+		console.log("Not part of start sequence");
 		return false;
-	}
-}
+	};
+
+	return self;
+};
 
 var currentPack = new Package();
 
 var ParsePackage = function(data){
-
 	for(i = 0; i < data.length; i++){
-		var currentByte = data.readInt8(i);
+		var currentByte = data[i];
+		console.log("Current byte: " + currentByte); // Let's see what we are reading
 		currentPack.AddByte(currentByte);
 		if (currentPack.IsFull()){
-			console.log(currentPack.GetBuffer())
+			console.log(currentPack.GetBuffer());
 			currentPack = new Package();
 		}
+		console.log() // newline
 	}
 };
