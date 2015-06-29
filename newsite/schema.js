f9af60c8f8784f88763e4cbdba7b5b5a7306c26d
Jesper Broge Jørgensen <jesperbroge@gmail.com>
2015-06-29 14:14:22 +0200

    Simplified schema

--- newsite/schema.js
+++ newsite/schema.js
@@ -3,56 +3,8 @@
 var _ = require('lodash');
 var Int64 = require('node-int64');
 
-var datatypes = {
-	boolean: 0,
-
-	uint8: 1,
-	int8: 2,
-	uint16: 3,
-	int16: 4,
-	uint32: 5,
-	int32: 6,
-	uint64: 7,
-	int64: 8,
-
-	float32: 9,
-	float64: 10,
-
-	cstring: 11,
-
-	utcDatetime: 12,
-};
-// Create the inverse
-_.forEach(datatypes, function(n, key) {
-	datatypes[n] = key;
-});
-
-var datatypeLength = {
-	boolean: 1,
-
-	uint8: 1,
-	int8: 1,
-	uint16: 2,
-	int16: 2,
-	uint32: 4,
-	int32: 4,
-	uint64: 8,
-	int64: 8,
-
-	float32: 4,
-	float64: 8,
-
-	cstring: 1,
-
-	utcDatetime: 8,
-
-	schema: 0,
-	schemaEnd: 0,
-};
-
-
 var ECUdata = {
-	"" : 0,
+	"Empty" : 0,
 	"Fuel Press.": 1,
 	"Status Lap Counter": 2,
 	"Status Injection Sum": 3,
@@ -100,139 +52,34 @@ _.forEach(ECUdata, function(n, key) {
 var pktTypes = {
 	ECU: 0,
 };
+pktTypes.ECU_END = pktTypes.ECU + _.size(ECUdata/2); // Half because its a two way map
 // Create the inverse
 _.forEach(pktTypes, function(n, key) {
 	pktTypes[n] = key;
 });
 
-var readInt64LE = function(buf, i) {
-	var lo = buf.readInt32LE(i);
-	var hi = buf.readInt32LE(i+4);
-	return new Int64(hi, lo);
-};
-
-var readPkt = function(buf) {
-	var pkt = [];
-	var i = 0;
-	while (i < buf.length) {
-		var dt = datatypes[buf.readInt8(i++)];
-		var data;
-		switch (dt) {
-			case "boolean": data = buf.readInt8(i) ? true : false; break;
-			case "int8":    data = buf.readInt8(i); break;
-			case "uint8":   data = buf.readUInt8(i); break;
-			case "int16":   data = buf.readInt16LE(i); break;
-			case "uint16":  data = buf.readUInt16LE(i); break;
-			case "int32":   data = buf.readInt32LE(i); break;
-			case "uint32":  data = buf.readUInt32LE(i); break;
-			case "int64":   data = readInt64LE(buf, i); break;
-			case "uint64":  throw new Error("not yet implemented");
-			case "float32": data = buf.readFloatLE(i); break;
-			case "float64": data = buf.readDoubleLE(i); break;
-
-			case "cstring": throw new Error("not yet implemented");
-			case "utcDatetime": data = new Date(readInt64LE(buf, i)); break;
-
-			default: throw new Error("Unknown datatype");
-		}
-		i += datatypeLength[dt];
-		pkt.push(data);
-	}
-	return pkt;
-};
-
-var parseECU = function(pkt) {
-	return {
-		'name': ECUdata[pkt.shift()],
-		data: pkt.shift(),
-		ts: pkt.shift(),
-	};
-};
-
-var unpack = function(buf) {
-	var pkt = readPkt(buf);
-
-	// console.log(pkt);
-
-	var type = pktTypes[pkt.shift()];
-	switch(type) {
-		case "ECU": return parseECU(pkt);
-
-		default: throw new Error("Unknown package type");
-	}
-};
-
-
-/*
-var parseSchema = function(input) {
-	var id = input[0];
-	var schema = {};
-	// schema[id] = {};
-
-	var body = []
-	input = input.slice(1);
-
-	var i = 0;
-	while (i < input.length) {
-		var iStart = i;
-		while(input[i++] != 0) {
-			if (i > input.length ) break;
-		}
-		var element = {};
-
-		var key = input.slice(iStart, i-1).toString(); // -1 because null terminator
-		element[key] = datatypes[input[i++]]
-		body.push(element);
-	}
-
-	var schemaStarts = [];
-	var schemaEnds = [];
-	_.forEach(body, function(e, n) {
-		var dt = _.values(e)[0];
-		switch (dt) {
-			case "schema": schemaStarts.push(n); break;
-			case "schema_end": schemaEnds.push(n); break;
-		}
-	})
-
-	if (schemaStarts.length !== schemaEnds.length) {
-		// ERROR
-		console.warn("mismatch error");
-	}
-
-	_(schemaStarts).reverse();
-	_(schemaEnds).reverse();
-
-	while (1) {
-		var start = schemaStarts.pop();
-		var end = schemaEnds.pop();
-		if (start === undefined || end === undefined) break;
-
-		var key = _.keys(body[start])[0];
-		body[start] = {};
-		body[start][key] = [];
-
-		_.forEach(_.range(start+1, end), function(n) {
-			body[start][key].push(body[n])
-		});
-		_.pullAt(body, _.range(start+1, end+1)); // TODO pull from nested data structure
-	}
-
-	schema[id] = body;
-	console.log(JSON.stringify(body, null, 2))
-	console.log();
-	console.log(body)
-}
-
-var test = function() {
-	var buf = new Buffer([0x01,0x73,0x63,0x68,0x65,0x6d,0x61,0x00,0x0d,0x6b,0x65,0x79,0x00,0x01,0x6b,0x65,0x79,0x32,0x00,0x02,0x00,0x0e]);
-	var buf2 = new Buffer([1, 115, 111, 109, 101, 32, 105, 110, 116, 0, 1, 115, 99, 104, 101, 109, 97, 0, 13, 107, 101, 121, 0, 1, 107, 101, 121, 50, 0, 2, 0, 14, 97, 110, 111, 116, 104, 101, 114, 32, 115, 99, 104, 101, 109, 97, 0, 13, 115, 111, 109, 101, 116, 104, 105, 110, 103, 0, 1, 78, 101, 115, 116, 101, 100, 32, 115, 99, 104, 101, 109, 97, 0, 13, 110, 101, 115, 116, 101, 100, 49, 0, 1, 110, 101, 115, 116, 101, 100, 50, 0, 1, 0, 14, 0, 14,]);
-	parseSchema(buf2);
-}
-
-test()
-*/
-
 module.exports = {
-	unpack: unpack,
+	unpack: function(buf) {
+		var pkts = [];
+		var i = 0;
+		while (i < buf.length) {
+			var pktType = buf.readUInt16LE(i);
+			i += 2;
+
+			if (pktType >= pktTypes.ECU || pktTypes <= pktTypes.ECU_END) {
+				var ecuPktType = pktType - pktTypes.ECU;
+				pkts.push({
+					name: ECUdata[ecuPktType],
+					value: buf.readFloatLE(i),
+				});
+				i += 4;
+			} else {
+				switch (pktType) {
+					default: throw "Unknown data type";
+				}
+			}
+
+		}
+		return pkts;
+	},
 };
